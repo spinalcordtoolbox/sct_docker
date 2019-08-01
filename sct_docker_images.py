3a03a7e7bdf105e0d152ee994cffce367152754b
Jérôme Carretero <cJ@zougloub.eu>
2019-08-01 19:06:32 -0400

    sct_docker_images: default build "official", don't build official and alias at same time

--- sct_docker_images.py
+++ sct_docker_images.py
@@ -3,11 +3,17 @@
 # Testing using Docker
 
 import sys, io, os, logging, time, datetime, shutil, datetime, subprocess
+import threading
+import multiprocessing.pool
 
 import sct_docker
 from sct_docker import printf, check_exe
 
+logger = logging.getLogger(__name__)
+
+
 default_distros = (
+ "official",
  #"ubuntu:14.04",
  "ubuntu:16.04",
  "ubuntu:18.04",
@@ -48,11 +54,13 @@ def generate(distros=None, version=None,
 	if version is None:
 		version = default_version
 
-	print("Generating distro Dockerfiles")
+	logger.info("Generating distro Dockerfiles")
 	names = []
 	for distro in distros:
 		name = "sct-{}-{}".format(version, distro.replace(":", "-")).lower()
-		printf("- %s..." % name)
+		logger.info("- %s...", name)
+
+		lock = threading.Lock() # prevent building official simultaneously to alias
 
 		name = sct_docker.generate(distro=distro, version=version,
 		 name=name, commands=default_commands,
@@ -64,13 +72,13 @@ def generate(distros=None, version=None,
 		 proxy=proxy,
 		)
 
-		names.append(name)
+		names.append((name, lock))
 
-		if distro == official_distro:
+		if distro == "official":
 			name = "sct-{}-{}".format(version, "official").lower()
-			printf("- %s..." % name)
+			logger.info("- %s...", name)
 
-			name = sct_docker.generate(distro=distro, version=version,
+			name = sct_docker.generate(distro=official_distro, version=version,
 			 name=name, commands=default_commands,
 			 install_fsleyes=True,
 			 #install_fsl=True,
@@ -79,48 +87,48 @@ def generate(distros=None, version=None,
 			 proxy=proxy,
 			)
 
-			names.append(name)
+			names.append((name, lock))
 
-		print("")
-	print("Done generating distro Dockerfiles")
+	logger.info("Done generating distro Dockerfiles")
 
-	print("Building images")
+	logger.info("Building images")
 
 	if not check_exe("docker"):
 		raise RuntimeError("You might want to have docker available when running this tool")
 
-	from multiprocessing.pool import ThreadPool
-	pool = ThreadPool(jobs)
+	pool = multiprocessing.pool.ThreadPool(jobs)
 
 	try:
 		res = list()
-		for name in names:
+		for name, lock in names:
 
 			cmd = [
 			 "docker", "build",
 			 "-t", name, name,
 			] + build_options
 
-			promise = pool.apply_async(lambda x: subprocess.call(x), (cmd,))
+			def docker_build(cmd, lock):
+				with lock:
+					return subprocess.call(cmd)
+
+			promise = pool.apply_async(docker_build, (cmd, lock))
 			res.append(promise)
 
 		errs = list()
-		for name, promise in zip(names, res):
+		for (name, _), promise in zip(names, res):
 			err = promise.get()
 			if err != 0:
 				logging.error("{} failed with error code {}".format(name, err))
 			errs.append(err)
 
 		pool.close()
-	except BaseException as e:
-		print("Keyboard interrupt")
+	finally:
 		pool.terminate()
-		raise SystemExit(1)
 	pool.join()
-	print("Done building images")
+	logger.info("Done building images")
 
 	failed = False
-	for name, err in zip(names, errs):
+	for (name,_), err in zip(names, errs):
 		if err == 0:
 			logging.info("{} finished successfully".format(name))
 		else:
@@ -128,50 +136,47 @@ def generate(distros=None, version=None,
 			failed = True
 
 	if failed:
-		print(errs)
-		logging.error("Not proceeding further as one distro failed")
-		return 1
+		logging.error("Not proceeding further as one distro failed: %s", errs)
+		raise RuntimeError("Failed generating one distro")
 
 	if proxy:
-		return 0
+		return
 
 	if publish_under:
-		print("Publishing on Docker hub")
-		for name in names:
-			printf("- %s..." % name)
+		logger.info("Publishing on Docker hub")
+		for name,_ in names:
+			logger.info("- %s...", name)
 			cmd = ["docker", "tag", name, "{}:{}".format(publish_under, name)]
 			subprocess.call(cmd)
 			cmd = ["docker", "push", "{}:{}".format(publish_under, name)]
 			subprocess.call(cmd)
-			print("")
-		print("Done publishing")
+
+		logger.info("Done publishing")
 
 	if generate_docker_tarball:
-		print("Generating Docker tarballs")
-		for name in names:
-			printf("- %s..." % name)
+		logger.info("Generating Docker tarballs")
+		for name,_ in names:
+			logger.info("- %s...", name)
 			cmd = ["bash", "-c", "docker save {}" \
 			 " | xz --threads=0 --best > {}-docker.tar.xz".format(name, name)]
 			subprocess.call(cmd)
-			print("")
-		print("Done generating Docker tarballs")
+		logger.info("Done generating Docker tarballs")
 
 	if generate_distro_specific_sct_tarball:
 
-		print("Generating offline archives")
+		logger.info("Generating offline archives")
 		if not (check_exe("xz") and check_exe("bash")):
 			raise RuntimeError("You might want to have bash & xz available when running this tool")
-		for name in names:
-			printf("- %s..." % name)
+		for name,_ in names:
+			logger.info("- %s...", name)
 			cmd = ["bash", "-c", "docker run --log-driver=none --entrypoint /bin/sh {} -c 'cd /home/sct; tar c sct_*'" \
 			 " | xz --threads=0 --best > {}-offline.tar.xz".format(name, name)]
 			subprocess.call(cmd)
-			print("")
-		print("Done generating offline archives")
+
+		logger.info("Done generating offline archives")
 
 
-if __name__ == "__main__":
-
+def main():
 	import argparse
 
 
@@ -196,7 +201,7 @@ if __name__ == "__main__":
 	subp.add_argument("--distros",
 	 nargs="+",
 	 help="Distributions to test (docker image names)",
-	 default=default_distros,
+	 default=["official"],
 	)
 
 	subp.add_argument("--version",
@@ -237,14 +242,17 @@ if __name__ == "__main__":
 	args = parser.parse_args()
 
 	if args.command == "generate":
-		res = generate(distros=args.distros, version=args.version,
+		return generate(distros=args.distros, version=args.version,
 		 generate_docker_tarball=args.generate_docker_tarball,
 		 generate_distro_specific_sct_tarball=args.generate_distro_specific_sct_tarball,
 		 publish_under=args.publish_under,
 		 jobs=args.jobs,
 		 proxy=args.proxy,
 		)
-		raise SystemExit(res)
 	else:
 		parser.print_help(sys.stderr)
-		raise SystemExit(1)
+		return 1
+
+if __name__ == "__main__":
+	res = main()
+	raise SystemExit(res)
